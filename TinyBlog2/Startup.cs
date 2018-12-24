d229238220020f629da0f7ff55c59baa8d3737c6
imwangji <86795863@qq.com>
2018-12-24 22:03:52 +0800

    6.允许跨域+配置文件

--- TinyBlog2/Startup.cs
+++ TinyBlog2/Startup.cs
@@ -29,7 +29,7 @@ namespace TinyBlog2
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            
+            services.AddCors();
             // 添加swaggerAPI文档生成工具
             services.AddSwaggerGen(c =>
             {
